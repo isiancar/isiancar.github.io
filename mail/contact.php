a66a2f365e8ee0606148a3070c6bd24a74cb1ab9
isiancar <136931133+isiancar@users.noreply.github.com>
2024-04-19 17:44:24 +0300

    Add files via upload

--- mail/contact.php
+++ mail/contact.php
@@ -9,9 +9,9 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "info@example.com"; // Change this email to your //
+$to = "info@nostaljidefteri.fun"; // Bu e-postayı kendi e-postanızla değiştirin //
 $subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$body = "Web sitenizin iletişim formundan yeni bir mesaj aldınız.\n\n"."İşte detaylar:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email";
 $header .= "Reply-To: $email";	
 
