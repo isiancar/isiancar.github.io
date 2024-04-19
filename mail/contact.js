ec5f122b2ac6d3f2e352086ac267988eb34930d4
isiancar <136931133+isiancar@users.noreply.github.com>
2024-04-19 18:09:42 +0300

    Update contact.js

--- mail/contact.js
+++ mail/contact.js
@@ -29,7 +29,7 @@ $(function () {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
                     $('#success > .alert-success')
-                            .append("<strong>Your message has been sent. </strong>");
+                            .append("<strong>Mesajınız gönderildi. </strong>");
                     $('#success > .alert-success')
                             .append('</div>');
                     $('#contactForm').trigger("reset");
