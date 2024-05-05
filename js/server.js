4b61cc38f22007100a83ef21987e998bbabba70a
isiancar <136931133+isiancar@users.noreply.github.com>
2024-05-05 17:20:15 +0300

    Create server.js

---
+++ js/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// MySQL veritabanı bağlantısı
+const connection = mysql.createConnection({
+    host: 'sql11.freesqldatabase.com',
+    user: 'sql11703078',
+    password: 'yJkjZE1LAp',
+    database: 'sql11703078' // Veritabanı adını değiştirin
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('MySQL connection error:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
+
+// Yeni yorum oluşturma endpoint'i
+app.post('/comments', (req, res) => {
+    const { name, email, website, message } = req.body;
+
+    // MySQL'ye yeni yorumu ekle
+    const sql = 'INSERT INTO comments (name, email, website, message) VALUES (?, ?, ?, ?)';
+    connection.query(sql, [name, email, website, message], (err, result) => {
+        if (err) {
+            console.error('MySQL query error:', err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            console.log('Yorum eklendi:', result);
+            res.status(201).send('Yorum başarıyla eklendi');
+        }
+    });
+});
+
+// Sunucuyu dinle
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
